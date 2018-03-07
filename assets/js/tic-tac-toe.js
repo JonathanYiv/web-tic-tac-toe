00b24cbceb95cfc17d4a1907dad67159b46c451f
Jonathan Yiv <JonathanYiv@gmail.com>
2018-03-07 10:27:37 -0500

    Visual Design

--- assets/js/tic-tac-toe.js
+++ assets/js/tic-tac-toe.js
@@ -7,3 +7,14 @@
 // JavaScript function to render the array on the webpage
 // JavaScript DOM event handlers to add marks to the board
 // JavaScript function to check for win logic
+const gameBoard = (() => {
+  const positions = new Array(9).fill(' ');
+  const placeO = pos => positions[pos] = 'O';
+  const placeX = pos => positions[pos] = 'X';
+  const getPositions = () => positions;
+  return {
+    placeO,
+    placeX,
+    getPositions,
+  };
+})();
